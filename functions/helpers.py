6dba757daa30605334e99d9bc51e3cb8383723b4
Ubuntu <ubuntu@ip-172-31-35-138.ec2.internal>
2020-10-26 15:52:14 +0000

    add instructions for s3 authentication

--- functions/helpers.py
+++ functions/helpers.py
@@ -1,7 +1,5 @@
 import os
 
-
-
 def wgs2epsg(x,y):
     # calculates EPSG code from xy coordinate in wgs84 format
     EPSG = int(32700-round((45+y)/90,0)*100+round((183+x)/6,0))
@@ -9,7 +7,7 @@ def wgs2epsg(x,y):
     
     return UTM_EPSG_code
 
-def makedir(dir_path):
+def make_dir(dir_path):
     # checks if a directory exists, if not create it.
         try:
             # Create  Directory  MyDirectory 
