bc461389fd22b73251f5cc945ab61cc8161a676f
Ubuntu <ubuntu@ip-172-31-35-138.ec2.internal>
2020-10-27 11:43:24 +0000

    update load helper functions

--- functions/helpers.py
+++ functions/helpers.py
@@ -1,5 +1,6 @@
-import os
 import numpy as np
+import requests
+import os
 
 def wgs2epsg(x,y):
     # calculates EPSG code from xy coordinate in wgs84 format
@@ -12,7 +13,7 @@ def make_dir(dir_path):
     # checks if a directory exists, if not create it.
         try:
             # Create  Directory  MyDirectory 
-            os.mkdir(dir_path)
+            os.makedirs(dir_path)
             #print if directory created successfully...
             print("Directory " , dir_path ,  " Created") 
         except FileExistsError:
@@ -22,3 +23,44 @@ def make_dir(dir_path):
 def reshape(index):
     index = np.reshape(index,[1,index.shape[0],index.shape[1]])
     return(index)
+
+def load(filename, input_shape=None):
+    np_image = Image.open(filename)
+    np_image = np.array(np_image).astype('float32')/255
+    np_image = transform.resize(np_image, input_shape)
+    np_image = np.expand_dims(np_image, axis=0)
+    return np_image
+
+def download_file(url, local):
+    
+    local_filename = local
+    
+    r = requests.get(url, stream=True)
+    
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                
+    return local_filename
+
+def check_valid_geometries(shapefile_path):
+    
+    shape_list = []
+    
+    # check crs
+    if fiona.open(shapefile_path).crs['init'] != 'epsg:4326':
+        print('crs is not epsg:4326, skipping...')
+        
+        crs_check = False
+    else:
+        crs_check = True
+        
+    data = []
+
+    for pol in fiona.open(shapefile_path):
+        if pol['geometry'] != None:
+                shape_list.append(pol)
+                data.append(pol)
+            
+    return shape_list, data, crs_check
